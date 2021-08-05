3bab95f7accb823b81bdbc7f63d14927fcdc7404
Komil <87507641+KomilUsman@users.noreply.github.com>
2021-08-05 16:06:20 -0400

    Added some cool code

--- src/SoftSkills.java
+++ src/SoftSkills.java
@@ -1,2 +1,8 @@
 public class SoftSkills {
+    public static void main(String[] args) {
+        System.out.println("What is agile:\n");
+        for (int i = 0; i <9 ; i++) {
+            System.out.println("In software development, agile is a set of practices intended to improve the effectiveness of software development professionals, teams, and organizations.");
+        }
+    }
 }
