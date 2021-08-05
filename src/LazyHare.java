d727e9814ba21e762bbd8038e1d21d5839809151
Altan-Ochir <altanochir0130@yahoo.com>
2021-08-05 14:39:04 -0500

    Added great news 1

--- src/LazyHare.java
+++ src/LazyHare.java
@@ -2,6 +2,11 @@ public class LazyHare {
 
     static {
 
-        System.out.println("this hare eat only ORGANIC CARROT");
+        System.out.println("this hare eat only ORGANIC CARROT,  yum yum ");
+
+    }
+
+    static {
+        System.out.println("This is something good");
     }
 }
