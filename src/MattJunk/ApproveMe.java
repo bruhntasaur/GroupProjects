22ff2e9a6aa8bbe971476331f8d077e3c762969c
Matthew K <wabasi333@gmail.com>
2021-08-06 09:49:56 -0500

    Adding MatthewsJunk Work Thing two.

--- src/MattJunk/ApproveMe.java
+++ src/MattJunk/ApproveMe.java
@@ -4,5 +4,6 @@ public class ApproveMe {
     public static void main(String[] args) {
         System.out.println("Do it.");
         System.out.println("This is a work thing, i made it");
+        System.out.println("Fine. if you say so. now its a third line, too much.");
     }
 }
