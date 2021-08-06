5d37edb1c3ba69f14910f24624135fb7dc15e932
Malika1201 <malika.lrjj@yahoo.com>
2021-08-06 12:35:16 -0500

    first branch commit

--- src/MalikasPackage/MalikasClass.java
+++ src/MalikasPackage/MalikasClass.java
@@ -3,5 +3,6 @@ package MalikasPackage;
 public class MalikasClass {
     public static void main(String[] args) {
         System.out.println("Hello Branch");
+        System.out.println("Some more code");
     }
 }
