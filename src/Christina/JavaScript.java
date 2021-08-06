4897122e8579d054d0369abd6702d91440844041
ChristinaDolganiuc <74195190+ChristinaDT@users.noreply.github.com>
2021-08-06 09:57:52 -0500

    New Request added

--- src/Christina/JavaScript.java
+++ src/Christina/JavaScript.java
@@ -3,5 +3,6 @@ package Christina;
 public class JavaScript {
     public static void main(String[] args) {
         System.out.println("Thanks God it's Saturday");
+        //Request accepted
     }
 }
