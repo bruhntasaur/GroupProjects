62f57df552a065efc1f6498f6242559e26c24107
Matthew K <wabasi333@gmail.com>
2021-08-07 11:17:32 -0500

    Adding some code to Christina Package

--- src/Christina/JavaScript.java
+++ src/Christina/JavaScript.java
@@ -3,6 +3,7 @@ package Christina;
 public class JavaScript {
     public static void main(String[] args) {
         System.out.println("Thanks God it's Saturday");
+        System.out.println("TGIF!");
         //Request accepted
     }
 }
